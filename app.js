c40097e1141753f58b6759608fe7298ba2e27a84
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com>
2024-09-05 17:18:43 +0500

    sign up page complete

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const path = require("path");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const pool = require("./db/pool");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(flash());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -18,7 +32,7 @@ app.get("/signup", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { messages: req.flash() });
 });
 
 app.get("/secret_key", (req, res) => {
@@ -27,6 +41,58 @@ app.get("/secret_key", (req, res) => {
 
 app.get("/dashboard", (req, res) => {
   res.render("dashboard", { user: "Work" });
+});
+
+app.post("/signup", async (req, res) => {
+  let { firstName, lastname, email, password, confPass } = req.body;
+  let errors = [];
+
+  if (!firstName || !lastname || !email || !password || !confPass) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Password should at least be 6 characters" });
+  }
+
+  if (password !== confPass) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    return res.render("signup", { errors });
+  }
+
+  // Form validation has passed
+  let hashedPassword = await bcrypt.hash(password, 10);
+
+  pool.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [email],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      if (results.rows.length > 0) {
+        errors.push({ message: "Email already registered" });
+        return res.render("signup", { errors });
+      }
+
+      pool.query(
+        `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id, password`,
+        [firstName, lastname, email, hashedPassword],
+        (err, results) => {
+          if (err) {
+            throw err;
+          }
+
+          req.flash("success_msg", "You are now registered. Please log in.");
+          res.redirect("/login");
+        }
+      );
+    }
+  );
 });
 
 const PORT = process.env.PORT || 3000;
--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,12 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-module.exports = new Pool({
-  host: process.env.DB_HOST,
+const pool = new Pool({
   user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 });
+
+module.exports = pool;
